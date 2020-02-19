38fe02df519f640a29e3807344be852c19bce865
Bushra-B <bushraxbil@gmail.com>
2020-02-19 08:52:35 +0200

    editted q7 switch, array spaces and new cases

--- js/app.js
+++ js/app.js
@@ -7,13 +7,13 @@ var q1, q2, q3, q4, q5, q6, q7 = '';
 var score = 0;
 var i;
 q1 = prompt('Q.1: Is Coffe My Favourite Drink?').toLowerCase();
-console.log('q1 user response is: ' + q1);
+//console.log('q1 user response is: ' + q1);
 switch(q1) {
 case 'yes':
 case 'y':
   alert('Correct!\nI love coffe! it is not just a favourite drink, it is one my favourite things in life!');
   score ++;
-  console.log('current score is: '+score);
+  //console.log('current score is: '+score);
   break;
 case 'no':
 case 'n':
@@ -21,14 +21,14 @@ case 'n':
   break;
 }
 q2 = prompt('Q.2: Do I love Reading?').toLowerCase();
-console.log('q2 user response is: ' + q2);
+//console.log('q2 user response is: ' + q2);
 switch(q2) {
 case 'yes':
 case 'y':
   alert('Right Guess!\nI love reading so much\nReading novels and literature is a hobby of mine.\nAnd I like reading\
 in general, whether it is articles, blogs or study materials.');
   score ++;
-  console.log('current score is: '+score);
+  //console.log('current score is: '+score);
   break;
 case 'no':
 case 'n':
@@ -37,7 +37,7 @@ articles, blogs and even study materials.');
   break;
 }
 q3 = prompt('Q.3: Is Blue My Favourite Color?').toLowerCase();
-console.log('q3 user response is: ' + q3);
+//console.log('q3 user response is: ' + q3);
 switch(q3) {
 case 'yes':
 case 'y':
@@ -49,18 +49,18 @@ case 'n':
   alert('Right, I am not a fan of this color and I do not like it that much.\n\
 My BEST color of all is Grean!\nI mean, how can one not LOVE Grean!');
   score ++;
-  console.log('current score is: '+score);
+  //console.log('current score is: '+score);
   break;
 }
 q4 = prompt('Q.4: Do You Think I Speak A Foreign Language - Other Than English - ?').toLowerCase();
-console.log('q4 user response is: ' + q4);
+//console.log('q4 user response is: ' + q4);
 switch(q4) {
 case 'yes':
 case 'y':
   alert('Corretto! Io parlo un po l\'italiano, e mi piacciono le lingue in generale - I speak a little Italian,\
  and I like languages in general.');
   score ++;
-  console.log('current score is: '+score);
+  //console.log('current score is: '+score);
   break;
 case 'no':
 case 'n':
@@ -69,14 +69,14 @@ case 'n':
   break;
 }
 q5 = prompt('Q.5: Am I Good At Coding?').toLowerCase();
-console.log('q5 user response is: ' + q5);
+//console.log('q5 user response is: ' + q5);
 switch(q5) {
 case 'yes':
 case 'y':
   alert('Thank you for your good opinion in me, '+userName+'! I am still a beginner learning to code\
 and I really hope I get to be very good at it.');
   score ++;
-  console.log('current score is: '+score);
+  //console.log('current score is: '+score);
   break;
 case 'no':
 case 'n':
@@ -89,32 +89,32 @@ for (i=1; i < 5; i++) {
   q6 = Number(prompt('Q.6: Guess how many sisters do I have?\
   \nNote: I will give you 4 attempts to answer this question.\nThis is attempt\
  number '+i));
-  console.log('q6 response is: '+q6);
+  //console.log('q6 response is: '+q6);
   if (q6 === 0) {
     alert('Right Guess! I have no sisters.\nBut I do have two amazing brothers!');
-    console.log('q6 response is: '+q6);
+    //console.log('q6 response is: '+q6);
     score ++;
-    console.log('current score is: '+score);
+    //console.log('current score is: '+score);
     correctAnswer = true;
     break;
   }
   else if (q6 > 0 && q6 <= 3) {
     alert('Not the correct answer, but you are close!');
-    console.log('q6 response is: '+q6);
+    //console.log('q6 response is: '+q6);
   }
   else if (q6 >= 4 && q6 <= 6) {
     alert('Not the correct answer, you\'r a little too far');
-    console.log('q6 response is: '+q6);
+    //console.log('q6 response is: '+q6);
   }
   else if (q6 >= 7) {
     alert('Not the correct answer, you\'r too far!');
-    console.log('q6 response is: '+q6);
+    //console.log('q6 response is: '+q6);
   }
 }
 if (correctAnswer === false) {
   alert('Sorry you have used all your attempts.\nThe correct answer is zero!\nI don\'t have any sisters :p');
 }
-var months = ['jan ', 'feb ', 'mar ', 'apr ', 'may ', 'jun ', 'jul ', 'aug ', 'sep ', 'oct ', 'nov ', 'dec '];
+var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 correctAnswer = false;
 forLoop:
 for (i=1; i < 7; i++) {
@@ -123,66 +123,78 @@ for (i=1; i < 7; i++) {
   switch(q7) {
   case months[0]:
   case 'january':
+  case '1':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[1]:
   case 'february':
+  case '2':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[2]:
   case 'march':
+  case '3':
     alert('Correct! I was born in March 29th');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     score ++;
-    console.log('current score is: '+score);
+    //console.log('current score is: '+score);
     correctAnswer = true;
     break forLoop;
   case months[3]:
   case 'april':
+  case '4':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[4]:
   case 'may':
+  case '5':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[5]:
   case 'june':
+  case '6':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[6]:
   case 'july':
+  case '7':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[7]:
   case 'august':
+  case '8':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[8]:
   case 'september':
+  case '9':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[9]:
   case 'october':
+  case '10':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[10]:
   case 'november':
+  case '11':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   case months[11]:
   case 'december':
+  case '12':
     alert('Not the right answer, try again!');
-    console.log('q7 response: '+q7);
+    //console.log('q7 response: '+q7);
     break;
   }
 }
